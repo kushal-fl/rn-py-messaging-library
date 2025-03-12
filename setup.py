ee631f4087b77a17cede902705a03be410c6cb3e
kushal-fl <87329315+kushal-fl@users.noreply.github.com>
2025-03-12 22:12:25 +0530

    Version upgrade

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='rn_py_messaging_library',
-    version='1.0.0',
+    version='1.0.1',
     packages=[
         'rabbitmq_client',
         'rabbitmq_client/async_connection',
